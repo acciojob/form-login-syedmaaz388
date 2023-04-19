54b29d371795e22f77e7c3eda713a01ce8a4ce49
syedmaaz388 <63224539+syedmaaz388@users.noreply.github.com>
2023-04-19 23:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
+  const firstName = document.forms["form1"]["fname"].value;
+  const lastName = document.forms["form1"]["lname"].value;
 
+  alert(`${firstName} ${lastName}`);
 }
